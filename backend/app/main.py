d8706fc72c13bfbf4cc834e8eb7b2a760efc335e
Ark Singh Shakya <83064348+Void-Ark@users.noreply.github.com>
2023-10-08 23:18:27 +0530

    connected to localhosted psql using psycopg2 python library

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException, status, Response
 from fastapi.params import Body
 
+import psycopg2 
+from psycopg2.extras import RealDictCursor
+from time import sleep
+
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
@@ -11,6 +15,25 @@ class Post(BaseModel) :
     published: bool = True 
     rating: Optional[int] = None 
     
+while True : 
+    try : 
+        conn = psycopg2.connect(
+            host='localhost',
+            dbname='Database-Ark', 
+            user='postgres', 
+            password='forgotansh1',
+            cursor_factory=RealDictCursor 
+            )
+        cursor = conn.cursor() 
+        print("the database connection is successful!!")
+        break 
+    except Exception as e : 
+        print(e) 
+        sleep(5)
+    
+
+    
+    
 DATA = [
     {'id':101 , 'title': 'first post', "content": 'data of first post', "published": True, "rating": 32},
     {'id':102 , 'title': 'second post', "content": 'data of second post', "published": False, "rating": 100},
@@ -19,54 +42,79 @@ DATA = [
 
 app = FastAPI() 
 
+#------------------------------GET ALL POST--------------------------
 @app.get(
     path='/post', 
     status_code=status.HTTP_200_OK)
 async def get_all_post() : 
-    return {"data": DATA}
+    cursor.execute('''SELECT * FROM posts''')
+    data = cursor.fetchall()
+    return data
 
+#------------------------------CREATE A POST --------------------------
 @app.post(
     path='/post', 
-    status_code=status.HTTP_201_CREATED) 
+    status_code=status.HTTP_201_CREATED)
 async def create_a_post(post: Post): 
-    data = post.model_dump()
-    data['id'] = randrange(3, 2034972034)
-    DATA.append(data) 
-    return DATA
+    cursor.execute(
+        query='''INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *''', 
+        vars=(post.title, post.content, post.published))
+    data = cursor.fetchone()
+    conn.commit()
+    return data
 
+#---------------------------GET A POST BY ID --------------------------
 @app.get(
     path='/post/{id}', 
     status_code=status.HTTP_302_FOUND)
 async def get_one(id: int): 
-    for i in DATA : 
-        if i['id'] == id : 
-            return i 
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f"Given {id} is not found")
+    cursor.execute(
+        query='''SELECT * FROM posts WHERE id = %s ''', 
+        vars=(id, ))
+    data = cursor.fetchone()
+    if data : 
+        return data     
+    else : 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Given {id} is not found")
 
+#------------------------DELETE A POST BY ID --------------------------
 @app.delete(
     path='/post/{id}', 
     status_code=status.HTTP_204_NO_CONTENT
 )
 async def delete_post_by_id(id: int):
-    for i in DATA : 
-        if i['id'] == id : 
-            DATA.remove(i)
-            return
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f"Given id {id} is not found")
+    cursor.execute(
+        query='''DELETE FROM posts where id = %s RETURNING *''', 
+        vars=(id, ))
+    
+    data = cursor.fetchone()
+     
+    if not data : 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"Given id {id} is not found")
+    
+    conn.commit() 
 
+
+#-------------------------UPDATE A POST BY ID ------------------------
 @app.put(
     path="/post/{id}",
-    status_code=status.HTTP_200_OK
-)
+    status_code=status.HTTP_200_OK)
 async def update_post_by_id(id: int, post: Post): 
-    print(post, type(post))
-    for i in DATA : 
-        if i['id'] == id : 
-            i['content'] = post.content
-            i['title'] = post.title 
-            return i
-    HTTPException(status_code=status.HTTP_404_NOT_FOUND)    
+    cursor.execute(
+        query='''UPDATE posts 
+                SET title = %s, content = %s, published = %s 
+                where id = %s returning *''', 
+        vars=(post.title, post.content, post.published, id))
+    data = cursor.fetchone()
+    
+    if data == None : 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"the given id {id} is not found!!")    
+    
+    conn.commit() 
+    return data
